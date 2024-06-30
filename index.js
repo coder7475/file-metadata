597c31baa38bff0fea0cbc832b25aecd39dfff3d
coder7475 <robiulhossain7475@gmail.com>
2024-06-30 20:11:55 +0600

    feat: add the file upload route

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", (req, res) => {
+  res.json({
+    file: "get",
+  });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Your app is listening on port " + port);
